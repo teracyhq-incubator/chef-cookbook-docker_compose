39daeef60fb0709d781d684ce50b30a0b2eaa288
Sebastian Boschert <sebastian.boschert@breuninger.de>
2016-08-15 11:13:56 +0200

    Added CentOS support and made sure the docker service is started in tests.

--- test/cookbooks/docker_compose_test/recipes/installation.rb
+++ test/cookbooks/docker_compose_test/recipes/installation.rb
@@ -4,8 +4,8 @@
 #
 # Copyright (c) 2016 Sebastian Boschert, All Rights Reserved.
 
-docker_installation 'default' do
- action :create
+docker_service 'default' do
+ action [:create, :start]
 end
 
 include_recipe 'docker_compose::installation'
