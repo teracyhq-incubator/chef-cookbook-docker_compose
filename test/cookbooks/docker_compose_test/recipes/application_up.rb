e5c9e63e25b2a95d32e7229be25ea838f047521c
Sebastian Boschert <sebastian@2007.org>
2016-08-15 11:13:56 +0200

    Added CentOS support and made sure the docker service is started in tests.

--- test/cookbooks/docker_compose_test/recipes/application_up.rb
+++ test/cookbooks/docker_compose_test/recipes/application_up.rb
@@ -4,13 +4,6 @@
 #
 # Copyright (c) 2016 Sebastian Boschert, All Rights Reserved.
 
-directory '/etc/docker-compose' do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '0750'
-end
-
 cookbook_file '/etc/docker-compose/docker-compose_nginx.yml' do
   action :create
   source 'docker-compose_nginx.yml'
